24eead937cab3934bc51e404f2f289fe6995f530
Gittnoob <peixitang@icloud.com>
2026-01-29 23:00:50 -0800

    changed env config

--- models.js
+++ models.js
@@ -1,11 +1,12 @@
 
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI;
-await mongoose.connect(uri, { dbName: 'websharer' });
+// const uri = process.env.MONGODB_URI;
+const MONGODB_URI="mongodb+srv://pexitang_db_user:xV9uyc9MowPP0Nqy@cluster0.ysjpteu.mongodb.net/?appName=Cluster0"
+await mongoose.connect(MONGODB_URI, { dbName: 'websharer' });
 
 
 const postSchema = new mongoose.Schema({
--- routes/api/v2/utils/urlPreviews.js
+++ routes/api/v2/utils/urlPreviews.js
@@ -7,13 +7,7 @@ async function getURLPreview(url){
       throw new Error('Missing url query parameter.');
     }
 
-    const response = await fetch(url, {
-      headers: {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'accept-language': 'en-US,en;q=0.9',
-      },
-    });
+    const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error(`Upstream request failed: ${response.status} ${response.statusText}`)
